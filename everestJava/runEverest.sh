9d1a3ba92bc6080574b04575333e3aab1675b37d
Paul Knight <paul.knight@delmarvacomputer.com>
2026-06-09 11:29:38 -0400

    Initialize copilot/generate-macos-executable

---
+++ everestJava/runEverest.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar ./Everest-Alpha-1.4.jar
---
+++ src/main/java/rocks/zipcode/everest/controllers/visualizers/Visualizer.java
@@ -0,0 +1,15 @@
+package rocks.zipcode.everest.controllers.visualizers;
+
+import javafx.scene.control.ScrollPane;
+
+public abstract class Visualizer extends ScrollPane {
+
+    Visualizer() {
+        setFitToHeight(true);
+        setFitToWidth(true);
+    }
+
+    public abstract void populate(String body) throws Exception;
+
+    public abstract void clear();
+}
